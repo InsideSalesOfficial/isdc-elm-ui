21ebb5a1b80369186321ac0814146de4178d82cb
Brian Cozzens <cozzbp@users.noreply.github.com>
2018-09-19 10:27:33 -0600

    Externalize checkbox from Dropdown (#7)
    * First Attempt
    
    * FirstAttempt
    
    * Move checkbox out
    
    * Fix tests
    
    * Fix name for test
    
    * More test fixes

---
+++ src/Isdc/Ui/Checkbox.elm
@@ -0,0 +1,70 @@
+module Isdc.Ui.Checkbox exposing (..)
+
+import Css exposing (..)
+import Html.Styled exposing (..)
+import Html.Styled.Attributes exposing (css, href, src, placeholder, value)
+import Html.Styled.Events exposing (onClick, onInput)
+import Isdc.Ui.Typography exposing (..)
+import Isdc.Ui.Icons exposing (..)
+import Isdc.Ui.Buttons exposing (..)
+import Isdc.Ui.Colors.Css exposing (..)
+import Isdc.Ui.Colors.Hex as Hex exposing (grayC)
+
+baseCheckboxStyles : Css.Style
+baseCheckboxStyles =
+    Css.batch
+        [ Css.width (px 18)
+        , Css.height (px 18)
+        , border2 (px 2) solid
+        , borderRadius (px 2)
+        , outline zero
+        , position relative
+        , marginRight (px 11)
+        , cursor pointer
+        , borderRadius (px 2)
+        ]
+
+type alias CheckboxOptions msg =
+    { checked : Bool
+    , disabled : Bool
+    , onValueChange : msg
+    , label: String
+    }
+
+checkBox : CheckboxOptions msg -> Html msg
+checkBox options =
+    div []
+        [ button
+            [ css
+                (if options.checked then
+                    [ backgroundColor green
+                    , baseCheckboxStyles
+                    , borderColor green
+                    ]
+                 else
+                    [ baseCheckboxStyles
+                    , borderColor black40
+                    ]
+                )
+              , onClick options.onValueChange
+            ]
+            (if options.checked then
+                [ span
+                    [ css
+                        [ borderBottom3 (px 2) solid white
+                        , borderLeft3 (px 2) solid white
+                        , width (px 10)
+                        , height (px 5)
+                        , transforms [ (translate2 (pct -50) (pct -65)), (rotate (deg -45)) ]
+                        , position absolute
+                        , top (pct 50)
+                        , left (pct 50)
+                        ]
+                    ]
+                    []
+                ]
+             else
+                []
+            )
+        , text options.label
+        ]
