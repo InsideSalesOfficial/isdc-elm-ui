3e446db1cd9eac277b57f69d39ceb63384d72038
gabe-lei <32003312+gabe-lei@users.noreply.github.com>
2018-09-20 15:37:20 -0600

    feat(icons):add chevron right icon (#15)
    * add chevron right icon
    
    * fix format, add arrow fill color param, code style change

--- src/Isdc/Ui/Icons.elm
+++ src/Isdc/Ui/Icons.elm
@@ -1,28 +1,17 @@
-module Isdc.Ui.Icons exposing
-    ( searchIcon
-    , addCircleIcon
-    )
+module Isdc.Ui.Icons exposing (searchIcon, addCircleIcon, chevronRightIcon)
 
 {-| SVG Icons
 
 
 # Icons
 
-@docs searchIcon
+@docs searchIcon, addCircleIcon, chevronRightIcon
 
 -}
 
 import Html
 import Svg exposing (..)
 import Svg.Attributes as Attr
-
-
-searchIconPath =
-    "M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"
-
-
-addCircleIconPath =
-    "M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm5 11h-4v4h-2v-4H7v-2h4V7h2v4h4v2z"
 
 
 iconBackgroundPath =
@@ -33,6 +22,10 @@ iconBackgroundPath =
 -}
 searchIcon : String -> String -> String -> Html.Html msg
 searchIcon w h f =
+    let
+        searchIconPath =
+            "M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"
+    in
     Svg.svg
         [ Attr.height h
         , Attr.width w
@@ -54,6 +47,10 @@ searchIcon w h f =
 
 addCircleIcon : String -> String -> String -> Html.Html msg
 addCircleIcon w h f =
+    let
+        addCircleIconPath =
+            "M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm5 11h-4v4h-2v-4H7v-2h4V7h2v4h4v2z"
+    in
     Svg.svg
         [ Attr.height h
         , Attr.width w
@@ -68,6 +65,32 @@ addCircleIcon w h f =
             []
         , path
             [ Attr.d addCircleIconPath
+            ]
+            []
+        ]
+
+
+chevronRightIcon : String -> String -> String -> String -> Html.Html msg
+chevronRightIcon w h f arrowColor =
+    let
+        chevronRightIconPath =
+            "M10 6L8.59 7.41 13.17 12l-4.58 4.59L10 18l6-6z"
+    in
+    Svg.svg
+        [ Attr.height h
+        , Attr.width w
+        , Attr.fill f
+        , Attr.viewBox "0 0 24 24"
+        ]
+        [ title [] [ text "Chevron Right Icon" ]
+        , path
+            [ Attr.d iconBackgroundPath
+            , Attr.fill "none"
+            ]
+            []
+        , path
+            [ Attr.d chevronRightIconPath
+            , Attr.fill arrowColor
             ]
             []
         ]
