b108c98a0c5b9e491d057b778175714bb610ac45
Aaron Wilson <aaron.wilson@insidesales.com>
2018-09-25 10:34:57 -0600

    Added radio and radioList UI functions

--- Makefile
+++ Makefile
@@ -1,9 +1,8 @@
-#Install uglifyjs and create-elm-app first
 createDocs:
-	cd ./docs && elm make ./src/Main.elm --optimize --output=./elm.js && uglifyjs elm.js --compress 'pure_funcs="F2,F3,F4,F5,F6,F7,F8,F9,A2,A3,A4,A5,A6,A7,A8,A9",pure_getters,keep_fargs=false,unsafe_comps,unsafe' | uglifyjs --mangle --output=elm.min.js && cd ../
+	cd ./docs && elm make ./src/Main.elm --optimize --output=./elm.js && ../node_modules/.bin/uglifyjs elm.js --compress 'pure_funcs="F2,F3,F4,F5,F6,F7,F8,F9,A2,A3,A4,A5,A6,A7,A8,A9",pure_getters,keep_fargs=false,unsafe_comps,unsafe' | ../node_modules/.bin/uglifyjs --mangle --output=elm.min.js && cd ../
 
 start:
-	cd ./docs && elm-app start
+	cd ./docs && ../node_modules/.bin/elm-app start
 
 test:
 	./node_modules/.bin/elm-test
---
+++ src/Isdc/Ui/Radio.elm
@@ -0,0 +1,94 @@
+module Isdc.Ui.Radio exposing (radio, radioList)
+
+import Css exposing (..)
+import Html.Styled exposing (..)
+import Html.Styled.Attributes exposing (css)
+import Html.Styled.Events exposing (onClick)
+import Isdc.Ui.Colors.Css as Colors
+import Isdc.Ui.Typography as Typography
+
+
+type alias Radio a msg =
+    { label : Html msg
+    , value : a
+    }
+
+
+radio : Radio a msg -> a -> (a -> msg) -> Html msg
+radio radioContent currentValue select =
+    let
+        { label, value } =
+            radioContent
+
+        selected =
+            currentValue == value
+    in
+    button
+        [ css
+            [ color Colors.black60
+            , displayFlex
+            , width <| pct 100
+            , outline zero
+            , padding2 (px 8) (px 10)
+            , justifyContent stretch
+            , Typography.body2
+            , margin2 (px 10) zero
+            , border zero
+            , cursor pointer
+            , focus
+                [ if selected then
+                    backgroundColor Colors.green40
+
+                  else
+                    backgroundColor Colors.green10
+                ]
+            , if selected then
+                backgroundColor Colors.green40
+
+              else
+                backgroundColor Colors.grayA
+            , firstChild
+                [ marginTop zero
+                ]
+            , lastChild
+                [ marginBottom zero
+                ]
+            ]
+        , onClick <| select value
+        ]
+        [ div
+            [ css
+                [ border3 (px 2) solid Colors.black
+                , borderRadius <| pct 50
+                , backgroundColor Colors.white
+                , marginRight <| px 10
+                , displayFlex
+                , alignItems center
+                , justifyContent center
+                , width <| px 20
+                , height <| px 20
+                , boxSizing borderBox
+                ]
+            ]
+          <|
+            if selected then
+                [ div
+                    [ css
+                        [ backgroundColor Colors.green
+                        , borderRadius <| pct 50
+                        , width <| px 10
+                        , height <| px 10
+                        ]
+                    ]
+                    []
+                ]
+
+            else
+                []
+        ]
+
+
+radioList : List (Radio a msg) -> a -> (a -> msg) -> Html msg
+radioList radios currentValue select =
+    div [] <|
+        List.map (\radioContent -> radio radioContent currentValue select) radios
